c57b72758d430b262ef041721eea34785448a504
ssljr <liujianrong@gmail.com>
2022-04-30 13:22:21 +0800

    使用python的类

---
+++ yoyo.py
@@ -0,0 +1,24 @@
+me = 'hello python world!'
+print(me)
+
+me = 'hello python land of mine'
+print(me.upper())
+
+name = 'sleep now'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = 'yoyo'
+last_name = 'liu'
+full_name = first_name + ' ' + last_name
+
+print(full_name)
+
+f_l = 'dave'
+l_n = 'jojo'
+full_n = f_l + ' ' + l_n
+
+line = 'Hello' + ' ' + full_n.title()
+
+print(line)
